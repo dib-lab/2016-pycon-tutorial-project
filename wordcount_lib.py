8df8919520e6f30d44c039ee52b81218d84829f5
Camille Scott <camille.scott.w@gmail.com>
2016-05-28 12:06:21 -0700

    Fix daaangerous code

--- wordcount_lib.py
+++ wordcount_lib.py
@@ -12,6 +12,6 @@ def consume(filename):
     return chars, words, lines
 
 def daaaangerous(param=0):
-    print("I'm the most dangerous function West of the Missippi, no test "\
+    print("I'm the most dangerous function West of the Mississippi, no test "\
           "will cover me!")
     return 3 / param
